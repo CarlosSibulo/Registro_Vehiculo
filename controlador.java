f08f22de3cfad3f9bad31a8b39ebc57806578312
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-04-11 06:19:54 -0500

    Create controlador.java

---
+++ controlador.java
@@ -0,0 +1,14 @@
+package tiendacarros;
+
+
+
+
+public class Controlador_Vehiculo {
+    private Vehiculo objVehiculo;
+    private formulario_Registrar_Vehiculo objformulario;
+    
+    private void Controlador(){
+    this.objVehiculo = new Vehiculo();
+    this.objformulario = new Formulario_Registrar_Vehiculo();
+    }
+}
