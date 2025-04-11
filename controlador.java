cdadf068bc8f5c0793dab712e94b0ae1a7f0b313
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-04-11 07:03:58 -0500

    Update controlador.java

--- controlador.java
+++ controlador.java
@@ -3,12 +3,12 @@ package tiendacarros;
 
 
 
-public class Controlador_Vehiculo {
-    private Vehiculo objVehiculo;
-    private formulario_Registrar_Vehiculo objformulario;
-    
-    private void Controlador(){
+public class vehiculo_controlador {
+   private void Controlador(){
     this.objVehiculo = new Vehiculo();
     this.objformulario = new Formulario_Registrar_Vehiculo();
+
+    private Vehiculo objVehiculo;
+    private formulario_Registrar_Vehiculo objformulario;
     }
 }
