b4a155bbcdbba244f413335ee4fe942835425ee7
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-04-11 06:23:52 -0500

    Create tienda_carro.java

---
+++ tienda_carro.java
@@ -0,0 +1,13 @@
+package tiendacarros;
+
+
+
+public class Controlador_1 {
+    private Usuario objUsuario;
+    private Formulario_Registro_Usuario objFormulario;
+    
+    public void Controlador(){
+    this.objUsuario = new Usuario();
+    this.objFormulario = new Formulario_Registro_usuario();
+    }
+}
