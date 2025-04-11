efe1409e2b0698f46dff58ff24625b2c60cf320b
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-04-11 06:18:36 -0500

    Create usuario.java

---
+++ usuario.java
@@ -0,0 +1,40 @@
+package tiendacarros;
+
+
+public class Usuario {
+    private String Nombre_usuario;
+    private String Correo_usuario;
+    private String Contraseña_usuario;
+    
+public void Usuario(){
+   String Nombre_Usuario = this.Nombre_usuario;
+   String Correo_Usuario = this.Correo_usuario;
+   String Contraseña_Usuario = this.Contraseña_usuario;
+
+}
+
+public String getNombre_usuario(){
+return Nombre_usuario;
+}
+
+public void setSeriel_vehiculo(String Nombre_usuario){
+this.Nombre_usuario = Nombre_usuario;
+}
+
+public String getCorreo_usuario(){
+return Correo_usuario;
+}
+
+public void setCorreo_usuario(String Correo_usuario){
+this.Correo_usuario = Correo_usuario;
+}
+
+public String getContraseña_usuario(){
+return Correo_usuario;
+}
+
+public void setContraseña_usuario(String Contraseña_usuario){
+this.Contraseña_usuario = Contraseña_usuario;
+} 
+
+}
